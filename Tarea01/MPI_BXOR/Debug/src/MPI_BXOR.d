25a26d313c87031bf0d99660ae1e54230d90c4dc
kattymmc <60157540+kattymmc@users.noreply.github.com>
2020-01-27 19:50:21 -0500

    Add files via upload

---
+++ Tarea01/MPI_BXOR/Debug/src/MPI_BXOR.d
@@ -0,0 +1,7 @@
+src/MPI_BXOR.o: ../src/MPI_BXOR.c \
+ /usr/lib/x86_64-linux-gnu/openmpi/include/mpi.h \
+ /usr/lib/x86_64-linux-gnu/openmpi/include/mpi_portable_platform.h
+
+/usr/lib/x86_64-linux-gnu/openmpi/include/mpi.h:
+
+/usr/lib/x86_64-linux-gnu/openmpi/include/mpi_portable_platform.h:
