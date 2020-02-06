aa0413ed88b8390bcde1b363ca46c207037080c5
kattymmc <60157540+kattymmc@users.noreply.github.com>
2020-02-06 16:41:07 -0500

    Add files via upload

---
+++ Solucionariopractica01/Ejercicio6/Debug/src/Ejercicio6.d
@@ -0,0 +1,7 @@
+src/Ejercicio6.o: ../src/Ejercicio6.c \
+ /usr/lib/x86_64-linux-gnu/openmpi/include/mpi.h \
+ /usr/lib/x86_64-linux-gnu/openmpi/include/mpi_portable_platform.h
+
+/usr/lib/x86_64-linux-gnu/openmpi/include/mpi.h:
+
+/usr/lib/x86_64-linux-gnu/openmpi/include/mpi_portable_platform.h:
